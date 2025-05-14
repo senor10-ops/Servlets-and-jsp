7169798e1248c646110a4ddcaaeabca3ae1c03dc
senor10-ops <ashwinpoudel08@gmail.com>
2025-05-14 10:17:03 +0545

    servlets

--- src/main/java/com/example/ComeServlet.java
+++ src/main/java/com/example/ComeServlet.java
@@ -20,7 +20,9 @@ public class ComeServlet extends HttpServlet {
                 //instead of using the default behavior in HttpServlet.
         
         // Set response content type
-        response.setContentType("text/html");
+        // response.setContentType("text/html");
+
+        // System.out.println("Hello from ComeServlet!");
         
         // Write HTML response
         PrintWriter out = response.getWriter(); //response is HTTP servlet response
@@ -28,9 +30,11 @@ public class ComeServlet extends HttpServlet {
         // printwriter(): character based output stream, useful for sending text based content
         //once you get printwriter object you can use it to write content that will be sent as a part of http response
         out.println("<html><body>");
-        out.println("<h1>Hello, Worldsn!</h1>");
+        out.println("<h1>Hello, Worldn!</h1>");
         out.println("</body></html>");
-        // request.getRequestDispatcher("/hellom.jsp").forward(request, response);
+        // request.getRequestDispatcher("/pages/hellom.jsp").forward(request, response);
+
+        // response.ser
 
     }
 }
